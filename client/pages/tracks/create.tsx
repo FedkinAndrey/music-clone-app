4c73d43d4248a18472972fe38151e1f02ba76778
FedkinAndrey <fedkin17@gmail.com>
2022-04-19 12:58:58 +0300

    added FileUpload for downloading audio and picture

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import MainLayout from "../../layouts/MainLayout";
 import StepWrapper from "../../components/StepWrapper";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
+import FileUpload from "../../components/FileUpload";
 
 const Create = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const [picture, setPicture] = useState(null);
+  const [audio, setAudio] = useState(null);
+
   const back = () => {
     setActiveStep((prevState) => prevState - 1);
   };
@@ -17,9 +21,23 @@ const Create = () => {
   return (
     <MainLayout>
       <StepWrapper activeStep={activeStep}>
-        {activeStep === 0 && <h1>Step 1</h1>}
-        {activeStep === 1 && <h1>Step 2</h1>}
-        {activeStep === 2 && <h1>Step 3</h1>}
+        {activeStep === 0 && (
+          <Grid container direction={"column"} style={{ padding: 20 }} gap={2}>
+            <TextField label="Track name" />
+            <TextField label={"Author name"} />
+            <TextField label={"Track text"} multiline rows={3} />
+          </Grid>
+        )}
+        {activeStep === 1 && (
+          <FileUpload setFile={setPicture} accept="image/*">
+            <Button>Download cover</Button>
+          </FileUpload>
+        )}
+        {activeStep === 2 && (
+          <FileUpload setFile={setAudio} accept="audio/*">
+            <Button>Download track</Button>
+          </FileUpload>
+        )}
       </StepWrapper>
       <Grid container justifyContent={"space-around"}>
         <Button disabled={activeStep === 0} onClick={back}>
